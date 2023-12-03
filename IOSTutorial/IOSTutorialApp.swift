5277e87f2f1ab3039a4db976f5cd22ddad7da6e8
Johny Alam <johny.alam@hublet.fi>
2023-12-03 05:17:29 +0200

    Initial Commit

---
+++ IOSTutorial/IOSTutorialApp.swift
@@ -0,0 +1,32 @@
+//
+//  IOSTutorialApp.swift
+//  IOSTutorial
+//
+//  Created by johny alam on 3.12.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct IOSTutorialApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
